00b9f1db7be004221a81194f4f64c6773998d6db
JayasinghePS <127961538+JayasinghePS@users.noreply.github.com>
2025-09-30 17:41:17 +0530

    About me part initial look

--- src/components/home/about/About.js
+++ src/components/home/about/About.js
@@ -6,6 +6,33 @@ function About() {
     <>
         <section className='about'>
         <h1>About</h1>
+      <div className="about-container">
+        {/* Left Column - Photo */}
+        <div className="about-photo">
+          <img 
+            src="/Assets/photo_with_no_background.png" 
+            alt="About Me" 
+          />
+        </div>
+
+        {/* Right Column - Description */}
+       <div className="about-description">
+        <p className="intro-hi">&lt; Hola, amigos! /&gt;</p>
+
+        <h1 className="intro-name">I’m Prageeth Jayasinghe!</h1>
+        <p className="intro-text">
+          A passionate software engineer who loves 
+          building scalable web applications and exploring new technologies. 
+          I enjoy working on both frontend and backend, and I’m always curious 
+          to learn more and improve my skills.
+        </p>
+        <div className="intro-buttons">
+        <button className='intro-cvbutton'>Download CV</button>
+        <button className='intro-contactbutton'>Contact me</button>
+        </div>
+      </div>
+
+      </div>
         </section>
       
     </>
